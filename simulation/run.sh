076fe737608734dcb54b4152e7897a5fb57bf00b
LuckMonkeys <duoduow1321@gmail.com>
2023-09-19 14:02:13 +0800

    update run.sh

--- simulation/run.sh
+++ simulation/run.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-python simulator.py -e='Venus_Sept' -t='./data/Venus' --sweep
+# python simulator.py -e='Venus_Sept' -t='./data/Venus' --sweep
+# 
+# 
+
+# python simulator.py -s lucid-node-scale --node_scaling_num=0
+# python simulator.py -s lucid-node-scale --node_scaling_num=1
+# python simulator.py -s lucid-node-scale --node_scaling_num=2
+# python simulator.py -s lucid-node-scale --node_scaling_num=3
+# 
+
+# vc_nodes_factor=(0.5 1.5 2.0 2.5 3.0)
+# vc_nodes_factor=(1.5 2.0 2.5 3.0)
+# vc_nodes_factor=(10.0 20.0 30.0)
+vc_nodes_factor=(0.9 0.7)
+
+for factor in "${vc_nodes_factor[@]}"
+do
+    python simulator.py -s lucid --vc_nodes_factor=$factor
+    python simulator.py -s lucid-alwaysgpu --vc_nodes_factor=$factor
+done
+
+
