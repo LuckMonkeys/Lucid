bdd85dc4dc533fa9769e91889c6262c295a5141a
LuckMonkeys <duoduow1321@gmail.com>
2023-11-07 15:35:12 +0800

    update search config generate

--- simulation/search_config_generate.py
+++ simulation/search_config_generate.py
@@ -6,7 +6,7 @@ import os
 
 dataset = "Venus"
 vc_nums = f"./data/{dataset}/vc_config.csv"
-config_dir = "./data/Venus/config"
+config_dir = f"./data/{dataset}/config"
 
 if not os.path.exists(config_dir):
     os.makedirs(config_dir)
@@ -15,8 +15,6 @@ vc_df = pd.read_csv( vc_nums, index_col=0)
 vcs = vc_df.index.tolist()
 
 vc_filter = ["vcvGl", "vcHvQ", "vcJsw"]
-vc_trace_scale = {"vcvGl":5, "vcHvQ":5, "vcJsw":5}
-vc_cluster_scale = {"vcvGl":2, "vcHvQ":2, "vcJsw":2}
 
 for vc in vc_filter:
     if vc not in vcs:
@@ -24,17 +22,23 @@ for vc in vc_filter:
 
 
 schedulers = ['lucid-fixed', 'lucid-continue', 'lucid-nogpu', 'lucid-alwaysgpu']
-thresholds = [0.1, 0.2, 0.3, 0.4]
+# thresholds = [0.1, 0.2, 0.3, 0.4]
+thresholds = [0.25]
+scale = [1,2,5,10]
 
     
 
-for i, (scheduler, threshold) in enumerate(product(schedulers, thresholds)) :
+for i, (scheduler, scale, threshold) in enumerate(product(schedulers, scale, thresholds)) :
     scheduler_dict = {vc:scheduler for vc in vc_filter}
     threshold_dict = {vc: threshold for vc in vc_filter}
+    trace_scale = {vc: scale for vc in vc_filter}
+    cluster_scale = {vc: scale for vc in vc_filter}
     
-    vc_config = {"threshold":threshold_dict, "scheduler":scheduler_dict}
-    vc_config["trace_scale"] = vc_trace_scale
-    vc_config["cluster_scale"] = vc_cluster_scale
+    vc_config = {"threshold":threshold_dict, 
+                "scheduler":scheduler_dict,
+                "trace_scale":trace_scale,
+                "cluster_scale":cluster_scale
+                }
     
-    f = open(f"./data/Venus/config/{i}.yaml", 'w')
+    f = open(f"./data/{dataset}/config/{i}.yaml", 'w')
     yaml.dump(vc_config, f)
